96ff15f5f525a8e5ab53f9222dcf8e0a2407573f
Bogdan Gochev <bogdan.dgochev@gmail.com>
2019-04-07 20:40:45 +0100

    separated memory interface in read and write

--- logdb/src/main/java/org/borer/logdb/bit/Memory.java
+++ logdb/src/main/java/org/borer/logdb/bit/Memory.java
@@ -1,47 +1,7 @@
 package org.borer.logdb.bit;
 
-public interface Memory
+public interface Memory extends ReadMemory, WriteMemory
 {
-    byte[] getSupportByteArrayIfAny();
-
-    long getBaseAddress();
-
-    void resetPosition();
-
-    long getCapacity();
-
-    void putLong(long value);
-
-    void putLong(long offset, long value);
-
-    long getLong();
-
-    long getLong(long offset);
-
-    void putInt(int value);
-
-    void putInt(long offset, int value);
-
-    int getInt();
-
-    int getInt(long offset);
-
-    void getBytes(byte[] array);
-
-    void getBytes(long sourceOffset, byte[] destinationArray);
-
-    void putBytes(byte[] sourceArray);
-
-    void putBytes(long destinationOffset, byte[] sourceArray);
-
-    void putByte(byte b);
-
-    void putByte(long offset, byte b);
-
-    byte getByte();
-
-    byte getByte(long offset);
-
     void assertBounds(long requestOffset, int requestLength);
 
     void assertBounds(long requestOffset, long requestLength);
