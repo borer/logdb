116e10db6b4e2d2b0323b8fd6fe5978be299a9f1
Bogdan Gochev <bogdan.dgochev@gmail.com>
2019-06-01 14:27:03 +0100

    added a simple abstraction on top of java crc32 checksum

---
+++ logdb/src/main/java/org/logdb/bit/ChecksumUtil.java
@@ -0,0 +1,66 @@
+package org.logdb.bit;
+
+import java.util.zip.CRC32;
+
+public class ChecksumUtil
+{
+    private final CRC32 checksumer = new CRC32();
+    private byte[] longToBytesBuffer = new byte[Long.BYTES];
+
+    public int calculateSingleChecksum(final byte[] buffer, final int offset, final int length)
+    {
+        checksumer.reset();
+        checksumer.update(buffer, offset, length);
+
+        return (int)checksumer.getValue();
+    }
+
+    public boolean compareSingleChecksum(final int originalChecksum,
+                                         final byte[] buffer,
+                                         final int offset,
+                                         final int length)
+    {
+        checksumer.reset();
+        checksumer.update(buffer, offset, length);
+
+        final int calculated = (int)checksumer.getValue();
+
+        return Integer.compareUnsigned(originalChecksum, calculated) == 0;
+    }
+
+    public void updateChecksum(final byte[] buffer, final int offset, final int length)
+    {
+        checksumer.update(buffer, offset, length);
+    }
+
+    public void updateChecksum(final long value)
+    {
+        final byte[] bytes = longToBytes(value);
+        checksumer.update(bytes, 0, bytes.length);
+    }
+
+    public int getAndResetChecksum()
+    {
+        final int value = (int) checksumer.getValue();
+        checksumer.reset();
+
+        return value;
+    }
+
+    private byte[] longToBytes(final long value)
+    {
+        long mutableValue = value;
+        for (int i = longToBytesBuffer.length - 1; i >= 0; i--)
+        {
+            longToBytesBuffer[i] = (byte)(mutableValue & 0xFF);
+            mutableValue >>= Byte.SIZE;
+        }
+
+        return longToBytesBuffer;
+    }
+
+    public boolean compareChecksum(final int checksum1, final int checksum2)
+    {
+        return Integer.compareUnsigned(checksum1, checksum2) == 0;
+    }
+}
---
+++ logdb/src/test/java/org/logdb/bit/ChecksumUtilTest.java
@@ -0,0 +1,61 @@
+package org.logdb.bit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ChecksumUtilTest
+{
+    private ChecksumUtil checksumUtil;
+    private byte[] buffer;
+
+    @BeforeEach
+    void setUp()
+    {
+        buffer = "this is a test".getBytes();
+        checksumUtil = new ChecksumUtil();
+    }
+
+    @Test
+    void shouldCompareCorrectlySameData()
+    {
+        final int originalChecksum = checksumUtil.calculateSingleChecksum(
+                buffer, 0, buffer.length);
+
+        assertTrue(checksumUtil.compareSingleChecksum(
+                originalChecksum,
+                buffer,
+                0,
+                buffer.length));
+    }
+
+    @Test
+    void shouldCompareCorrectlyDifferentData()
+    {
+        final int originalChecksum = checksumUtil.calculateSingleChecksum(
+                buffer, 0, buffer.length);
+        final int skipElements = 1;
+
+        assertFalse(checksumUtil.compareSingleChecksum(
+                originalChecksum,
+                buffer,
+                skipElements,
+                buffer.length - skipElements));
+    }
+
+    @Test
+    void shouldCompareCorrectlyOverMultipleAppends()
+    {
+        checksumUtil.updateChecksum(buffer, 0, buffer.length);
+        checksumUtil.updateChecksum(1234L);
+        final int checksum1 = checksumUtil.getAndResetChecksum();
+
+        checksumUtil.updateChecksum(buffer, 0, buffer.length);
+        checksumUtil.updateChecksum(1234L);
+        final int checksum2 = checksumUtil.getAndResetChecksum();
+
+        assertTrue(checksumUtil.compareChecksum(checksum1, checksum2));
+    }
+}
